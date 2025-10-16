88fd3481fb2b4aab2e9ba36cb56eb79beacc4b4a
Bri Stanback <233396+Stanback@users.noreply.github.com>
2025-10-16 11:51:06 -0700

    chore: ghostty config

--- zsh/.oh-my-zsh/custom/macos.zsh
+++ zsh/.oh-my-zsh/custom/macos.zsh
@@ -1,7 +1,12 @@
 # macOS-specific configuration
 
 # iTerm2 Shell Integration
-if [ ! -e ~/.iterm2_shell_integration.zsh ]; then
-  curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
+# if [ ! -e ~/.iterm2_shell_integration.zsh ]; then
+#   curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
+# fi
+# source ~/.iterm2_shell_integration.zsh
+
+# Ghostty integration (only when inside Ghostty)
+if [[ -n $GHOSTTY_RESOURCES_DIR ]]; then
+  source "$GHOSTTY_RESOURCES_DIR/shell-integration/zsh/ghostty-integration"
 fi
-source ~/.iterm2_shell_integration.zsh
